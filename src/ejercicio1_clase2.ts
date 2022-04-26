5f0894b4253363e79448587d005ec18c6acd6bc7
Gisela-Gentile <104107443+Gisela-Gentile@users.noreply.github.com>
2022-04-26 15:24:46 -0300

    Initial commit

---
+++ src/ejercicio1_clase2.ts
@@ -0,0 +1,31 @@
+//jercicio 1: varibles ensecuencia
+let titulo: string = " Torta de aceite";
+let paso1: string = "1- Batir en un bol 1 taza de aceite con una taza de azucar";
+let paso2: string = "2- Agregar 3 huevos de a uno a la vez";
+let paso3: string = "3- Incorporar 2 tazas de harina leudante de apoco";
+let paso4: string = "4- Agregar ralladura de un limón";
+let paso5: string = "5- Colocar en un molde para torta enmantecado";
+let paso6: string = "6- Hornear durante 40 minutos";
+let paso7: string = "7- Dejar enfriar y desmoldar";
+let paso8: string = "8- Decorar";
+
+
+
+
+console.log(titulo);
+console.log(paso1);
+console.log(paso2);
+console.log(paso3);
+console.log(paso4);
+console.log(paso5);
+console.log(paso6);
+console.log(paso7);
+console.log(paso8);
+
+
+
+//ejercicio 2: area rectangulo
+let base : number = 5;
+let altura : number = 10;
+let resultado : number = base * altura;
+console.log(resultado);
